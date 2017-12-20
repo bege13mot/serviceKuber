815287fd607fbdcde9330c622f9ed89ad08737b8
Ivan <ivan@Ivans-MacBook-Pro.local>
2017-12-20 13:21:25 +0400

    Step 8

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"./handlers"
+	"./version"
+)
+
+func main() {
+	log.Printf(
+		"Starting the service...\ncommit: %s, build time: %s, release: %s",
+		version.Commit, version.BuildTime, version.Release,
+	)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set")
+	}
+
+	r := handlers.Router(version.BuildTime, version.Commit, version.Release)
+	log.Print("The service is ready to listen and serve.")
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
